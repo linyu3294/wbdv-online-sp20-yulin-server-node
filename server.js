9b7d6e9950b50249ad5fdbaaaeb56d2941ae2290
Yu Lin <linyu3294@gmail.com>
2020-04-09 20:17:58 -0400

    changed CORs to Heroku domain

--- server.js
+++ server.js
@@ -4,8 +4,10 @@ const app = express()
 
 //Configuring some parameters to specify how the express app will listen to any request
 app.use(function(req, res, next) {
+    // res.header("Access-Control-Allow-Origin",
+    //     "http://localhost:4200")
     res.header("Access-Control-Allow-Origin",
-        "http://localhost:4200")
+        "https://secret-plateau-50201.herokuapp.com")
     res.header("Access-Control-Allow-Headers",
         "Origin, X-Requested-With, Content-Type, Accept")
     res.header("Access-Control-Allow-Methods",
