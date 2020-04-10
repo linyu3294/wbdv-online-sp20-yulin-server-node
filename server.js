7e14b9f8412cfc55e31325c83edf96f7a3fc242c
Yu Lin <linyu3294@gmail.com>
2020-04-09 20:38:06 -0400

    commented out port

--- server.js
+++ server.js
@@ -6,8 +6,8 @@ const app = express()
 app.use(function(req, res, next) {
     // res.header("Access-Control-Allow-Origin",
     //     "http://localhost:4200")
-    res.header("Access-Control-Allow-Origin",
-        "https://secret-plateau-50201.herokuapp.com")
+    // res.header("Access-Control-Allow-Origin",
+    //     "https://secret-plateau-50201.herokuapp.com")
     res.header("Access-Control-Allow-Headers",
         "Origin, X-Requested-With, Content-Type, Accept")
     res.header("Access-Control-Allow-Methods",
