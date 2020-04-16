1809fe1e8d710cf13c4eb67f199092e93bc651b3
Yu Lin <linyu3294@gmail.com>
2020-04-16 14:05:29 -0400

    Assignment 9

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(function(req, res, next) {
     // res.header("Access-Control-Allow-Origin",
     //     "http://localhost:4200")
     res.header("Access-Control-Allow-Origin",
-        "https://wbdv-o-sp20-yulin-client-ng.herokuapp.com")
+        "https://wbdv-o-sp20-yulin-client-ng-2.herokuapp.com")
     res.header("Access-Control-Allow-Headers",
         "Origin, X-Requested-With, Content-Type, Accept")
     res.header("Access-Control-Allow-Methods",
