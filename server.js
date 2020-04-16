b47135cc1b21e11bbfc5d648a3eaed19fbccbcf2
Yu Lin <linyu3294@gmail.com>
2020-04-16 13:31:02 -0400

    asdfs

--- server.js
+++ server.js
@@ -19,16 +19,16 @@ app.use(function(req, res, next) {
 });
 
 
-// var connectionString = 'mongodb://127.0.0.1:27017/whiteboard-cs5610-o-ylin-db';
-// if(process.env.MLAB_USERNAME_WEBDEV) {
-//     var username = process.env.MLAB_USERNAME_WEBDEV;
-//     var password = process.env.MLAB_PASSWORD_WEBDEV;
-//     connectionString = 'mongodb://' + username + ':' + password;
-//     connectionString += '@ds051605.mlab.com:51605/heroku_wtlbsg56';
-// }
-//
-// mongoose.connect(connectionString,
-//     { useNewUrlParser: true, useUnifiedTopology: true })
+var connectionString = 'mongodb://127.0.0.1:27017/whiteboard-cs5610-o-ylin-db';
+if(process.env.MLAB_USERNAME_WEBDEV) {
+    var username = process.env.MLAB_USERNAME_WEBDEV;
+    var password = process.env.MLAB_PASSWORD_WEBDEV;
+    connectionString = 'mongodb://' + username + ':' + password;
+    connectionString += '@ds051605.mlab.com:51605/heroku_wtlbsg56';
+}
+
+mongoose.connect(connectionString,
+    { useNewUrlParser: true, useUnifiedTopology: true })
 
 // mongoose.connect("mongodb://heroku_wtlbsg56:lo3e688p0hdjlcof2nmuugl4ar@ds051605.mlab.com:51605/heroku_wtlbsg56",
 //     { useNewUrlParser: true, useUnifiedTopology: true })
@@ -39,12 +39,12 @@ app.use(function(req, res, next) {
 // app.use(bodyparser.json)
 
 
-// var session = require('express-session')
-// app.use(session({
-//     resave: false,
-//     saveUninitialized: true,
-//     secret: 'any string'
-// }));
+var session = require('express-session')
+app.use(session({
+    resave: false,
+    saveUninitialized: true,
+    secret: 'any string'
+}));
 
 require('./controllers/quizzes.controller.server')(app)
 require('./controllers/questions.controller.server')(app)
@@ -54,4 +54,5 @@ require('./controllers/session.controller.server')(app)
 app.get('/hello', (req, res) => res.send('hello world!'))
 
 app.listen(process.env.PORT)
+//app.listen(3000)
 
